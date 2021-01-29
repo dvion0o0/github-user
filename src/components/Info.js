c442791bafb21aff72dc306b46b807f33aac6bed
Dv <dv@Dvs-MacBook-Pro.local>
2021-01-29 23:45:31 +0530

    info component completed

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,11 +1,65 @@
-import React from 'react';
-import { GithubContext } from '../context/context';
-import styled from 'styled-components';
-import { GoRepo, GoGist } from 'react-icons/go';
-import { FiUsers, FiUserPlus } from 'react-icons/fi';
+import React from "react";
+import { GithubContext } from "../context/context";
+import styled from "styled-components";
+import { GoRepo, GoGist } from "react-icons/go";
+import { FiUsers, FiUserPlus, FiUser } from "react-icons/fi";
 
 const UserInfo = () => {
-  return <h2>user info component</h2>;
+  const { githubUser } = React.useContext(GithubContext);
+  const { public_repos, public_gists, following, followers } = githubUser;
+
+  const items = [
+    {
+      id: 1,
+      label: "repos",
+      value: public_repos,
+      color: "pink",
+      icon: <GoRepo className="icom"></GoRepo>,
+    },
+    {
+      id: 2,
+      label: "followers",
+      value: followers,
+      color: "green",
+      icon: <FiUser className="icom" />,
+    },
+    {
+      id: 3,
+      label: "following",
+      value: following,
+      color: "purple",
+      icon: <FiUserPlus className="icom" />,
+    },
+    {
+      id: 4,
+      label: "gists",
+      value: public_gists,
+      color: "yellow",
+      icon: <GoGist className="icom"></GoGist>,
+    },
+  ];
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {items.map((item) => {
+          return <Item key={item.id} {...item} />;
+        })}
+      </Wrapper>
+    </section>
+  );
+};
+
+const Item = ({ icon, label, value, color }) => {
+  return (
+    <article className="item">
+      <span className={color}>{icon}</span>
+      <div>
+        <h3>{value}</h3>
+        <p>{label}</p>
+      </div>
+    </article>
+  );
 };
 
 const Wrapper = styled.section`
