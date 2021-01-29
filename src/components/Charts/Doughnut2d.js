76f302133d317732d19f103c96228942a5bc6540
Dv <dv@Dvs-MacBook-Pro.local>
2021-01-30 03:29:49 +0530

    charts added

--- src/components/Charts/Doughnut2d.js
+++ src/components/Charts/Doughnut2d.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React from "react";
 
-const Doughnut2d = () => {
-  return <div>chart</div>;
+import ReactFC from "react-fusioncharts";
+
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Column2D from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+const Doughnut2d = ({ data }) => {
+  const chartConfigs = {
+    type: "doughnut2d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Stars Per Language",
+        decimals: 0,
+        theme: "fusion",
+        doughnutRadius: "45%",
+        showPercentValues: 0,
+        showLegend: 0,
+      },
+      // Chart Data
+      data: data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
 export default Doughnut2d;
--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import React from "react";
+import ReactFC from "react-fusioncharts";
 
-const Pie3D = () => {
-  return <div>chart</div>;
+// Include the fusioncharts library
+import FusionCharts from "fusioncharts";
+
+// Include the chart type
+import Column2D from "fusioncharts/fusioncharts.charts";
+
+// Include the theme as fusion
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
+
+// Adding the chart and theme as dependency to the core fusioncharts
+ReactFC.fcRoot(FusionCharts, Column2D, FusionTheme);
+
+const Pie3D = ({ data }) => {
+  const chartConfigs = {
+    type: "pie3d", // The chart type
+    width: "100%", // Width of the chart
+    height: "400", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "Languages",
+        numberSuffix: "%",
+        theme: "fusion",
+        pieRadius: "35%",
+      },
+      // Chart Data
+      data: data,
+    },
+  };
+
+  return <ReactFC {...chartConfigs} />;
 };
 
 export default Pie3D;
